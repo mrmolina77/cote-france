02f30fc4083aaeef23d37aaa5fafa8c6704e7d2d
Mauro Molina <mrmolina77@gmail.com>
2024-06-11 22:33:10 -0400

    Ya funciona el boton modificar y se cambio el flujo

--- app/Http/Livewire/ShowProspectos.php
+++ app/Http/Livewire/ShowProspectos.php
@@ -4,23 +4,47 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Prospecto;
+use App\Models\Origen;
+use App\Models\Seguimiento;
+use App\Models\Estatu;
 
 class ShowProspectos extends Component
 {
     public $search = "";
     public $sort = 'prospectos_id';
     public $direction = 'asc';
-    
+    public $prospecto;
+
+    public $open_edit = false;
     protected $listeners = ['render' ];
-    
+
+    protected $rules = [
+        'prospecto.prospectos_nombres'=>'required|min:3|max:50',
+        'prospecto.prospectos_apellidos'=>'required|min:3|max:50',
+        'prospecto.prospectos_telefono'=>'required|numeric',
+        'prospecto.prospectos_correo'=>'required|email|max:100',
+        'prospecto.origenes_id'=>'required',
+        'prospecto.seguimientos_id'=>'required',
+        'prospecto.estatus_id'=>'required',
+        'prospecto.prospectos_comentarios'=>'required|min:7|max:255',
+        'prospecto.prospectos_fecha'=>'required|date',
+    ];
+
     public function render()
     {
         $prospectos = Prospecto::where('prospectos_nombres','like','%'.trim($this->search).'%')
                                ->orWhere('prospectos_apellidos','like','%'.trim($this->search).'%')
                                ->orderBy($this->sort,$this->direction)
                                ->get();
-        return view('livewire.show-prospetos',['prospectos'=>$prospectos]);
+        $origenes = Origen::all();
+        $seguimientos = Seguimiento::all();
+        $estatus = Estatu::all();
+        return view('livewire.show-prospetos',['prospectos'=>$prospectos
+                                              ,'origenes'=>$origenes
+                                              ,'seguimientos'=>$seguimientos
+                                              ,'estatus'=>$estatus]);
     }
+
     public function order($order){
         if ($this->sort== $order) {
             if ($this->direction == 'desc') {
@@ -32,9 +56,17 @@ class ShowProspectos extends Component
             $this->sort= $order;
             $this->direction = 'asc';
         }
-        
+    }
 
-        
+    public function edit(Prospecto $prospecto){
+        $this->prospecto = $prospecto;
+        $this->open_edit = true;
+    }
+
+    public function update(){
+        $this->prospecto->save();
+        $this->reset(['open_edit']);
+        $this->emit('alert','El prospecto fue modificado satifactoriamente');
 
     }
 }
--- resources/views/livewire/show-prospetos.blade.php
+++ resources/views/livewire/show-prospetos.blade.php
@@ -9,7 +9,7 @@
         <div class="px-6 py-4">
             <x-forms.input type="text" class="flex-1 ml-4" wire:model="search"/>
         </div>
-        <x-table>            
+        <x-table>
             <table class="items-center bg-transparent w-full border-collapse ">
                 <thead>
                 <tr>
@@ -76,7 +76,7 @@
 
                 <tbody>
                 @forelse ( $prospectos as $item )
-                    
+
                 <tr>
                     <th class="border-t-0 px-2 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
                         {{$item->prospectos_id}}
@@ -94,7 +94,7 @@
                         {{$item->prospectos_telefono}}
                     </td>
                     <td class="flex border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        @livewire('edit-prospecto',['item' => $item, key($item->prospectos_id)])
+                        <i class="fas fa-pen text-emerald-500 mr-4" wire:click="edit({{ $item }})"></i>
                         <i class="fas fa-trash text-red-500 mr-4"></i>
                     </td>
                 </tr>
@@ -105,11 +105,116 @@
                     </th>
                 </tr>
                 @endforelse
-                
+
                 </tbody>
 
             </table>
         </x-table>
     </div>
+
+    <x-dialog-modal wire:model="open_edit">
+        <x-slot name="title">
+            Actualizar prospecto
+        </x-slot>
+        <x-slot name="content">
+            <div>
+                <div class="mb-4 flex">
+                    <x-forms.label value="Nombres: " />
+                    <x-forms.input type="text" class="flex-1 ml-4" wire:model="prospecto.prospectos_nombres"/>
+                </div>
+                <x-forms.input-error for="prospectos_nombres"/>
+           </div>
+            <div>
+                <div class="mb-4 flex">
+                    <x-forms.label value="Apellidos: " />
+                    <x-forms.input type="text" class="flex-1 ml-4" wire:model="prospecto.prospectos_apellidos"/>
+                </div>
+                <x-forms.input-error for="prospectos_apellidos"/>
+            </div>
+            <div>
+                <div class="mb-4 flex">
+                    <x-forms.label value="Telefono: " />
+                    <x-forms.input type="text" class="flex-1 ml-4" wire:model="prospecto.prospectos_telefono"/>
+                </div>
+                <x-forms.input-error for="prospectos_telefono"/>
+            </div>
+            <div>
+                <div class="mb-4 flex">
+                    <x-forms.label value="Correo: " />
+                    <x-forms.input type="text" class="flex-1 ml-4" wire:model="prospecto.prospectos_correo"/>
+                </div>
+                <x-forms.input-error for="prospectos_correo"/>
+            </div>
+            <div>
+                <div class="mb-4 flex">
+                    <x-forms.label value="Origenes: " />
+                    <x-select class="flex-1 ml-4" wire:model="prospecto.origenes_id">
+                        <option value="">Seleccionar</option>
+                        @forelse ($origenes as $item)
+                        <option value="{{$item->origenes_id}}">{{$item->origenes_descripcion}}</option>
+                        @empty
+                        <option value="">Sin origenes</option>
+                        @endforelse
+                    </x-select>
+                </div>
+                <x-forms.input-error for="origenes_id"/>
+            </div>
+            <div>
+                <div class="mb-4 flex">
+                    <x-forms.label value="Seguimientos: " />
+                    <x-select class="flex-1 ml-4" wire:model="prospecto.seguimientos_id">
+                        <option value="">Seleccionar</option>
+                        @forelse ($seguimientos as $item)
+                        <option value="{{$item->seguimientos_id}}">{{$item->seguimientos_descripcion}}</option>
+                        @empty
+                        <option value="">Sin segumientos</option>
+                        @endforelse
+                    </x-select>
+                </div>
+                <x-forms.input-error for="seguimientos_id"/>
+            </div>
+            <div>
+                <div class="mb-4 flex" >
+                    <x-forms.label value="Origenes: " />
+                    <x-select class="flex-1 ml-4" wire:model="prospecto.estatus_id">
+                        <option value="">Seleccionar</option>
+                        @forelse ($estatus as $item)
+                        <option value="{{$item->estatus_id}}">{{$item->estatus_descripcion}}</option>
+                        @empty
+                        <option value="">Sin origenes</option>
+                        @endforelse
+                    </x-select>
+                </div>
+                <x-forms.input-error for="estatus_id"/>
+            </div>
+            <div>
+                <div class="mb-4 flex">
+                    <x-forms.label value="Comentario: " />
+                    <x-forms.textarea rows="4" class="flex-1 ml-4" wire:model="prospecto.prospectos_comentarios">
+                    </x-forms.textarea>
+                </div>
+                <x-forms.input-error for="prospectos_comentarios"/>
+            </div>
+            <div>
+                <div class="mb-4 flex">
+                    <x-forms.label value="Fecha: " />
+                    <x-forms.input type="date" class="flex-1 ml-4" wire:model="prospecto.prospectos_fecha"/>
+                </div>
+                <x-forms.input-error for="prospectos_fecha"/>
+            </div>
+        </x-slot>
+        <x-slot name="footer">
+            <x-forms.red-button wire:click="$set('open_edit',false)">
+                Cancelar
+            </x-forms.red-button>
+            <x-forms.blue-button wire:click="save"  wire:loading.attr="disabled" wire:click="update" class="disabled:opacity-65">
+                Modificar Prospecto
+            </x-forms.blue-button>
+            {{-- <span wire:loading wire:target="save">Cargando...</span> --}}
+        </x-slot>
+    </x-dialog-modal>
+
+
+
 
 </div>
