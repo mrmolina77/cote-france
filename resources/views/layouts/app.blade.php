b0156bd6a71f986169fd3633620e5072448d6076
Mauro Molina <mrmolina77@gmail.com>
2024-11-14 19:10:11 -0400

    Incidencia en  prospecto.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <!-- Styles -->
         <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
         <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
-
+        @stack('css')
         @livewireStyles
     </head>
     <body class="bg-gray-100 font-family-karla flex">
