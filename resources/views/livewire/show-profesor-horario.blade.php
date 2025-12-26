365134361ea6112f3b14832119257a1cd50bcfd1
Mauro Molina <mrmolina77@gmail.com>
2025-12-26 12:15:10 -0400

    Add professor-only schedule toggle

--- resources/views/livewire/show-profesor-horario.blade.php
+++ resources/views/livewire/show-profesor-horario.blade.php
@@ -51,6 +51,11 @@
                             Siguiente
                         </button>
                     </div>
+                    <div class="col-span-full">
+                        <button class="w-full py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" wire:click="toggleSoloProfesor">
+                            {{ $solo_profesor ? 'Ver todos los profesores' : 'Ver solo mis horarios' }}
+                        </button>
+                    </div>
                 </div>
             </div>
 
