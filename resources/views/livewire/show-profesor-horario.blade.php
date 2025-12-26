deea6000a6c1593d7758b9371bbdf034020bd296
Mauro Molina <mrmolina77@gmail.com>
2025-12-26 12:32:56 -0400

    Update horario toggle labels

--- resources/views/livewire/show-profesor-horario.blade.php
+++ resources/views/livewire/show-profesor-horario.blade.php
@@ -12,7 +12,7 @@
                     </div>
                     <div class="flex items-center justify-center">
                         <button class="w-full py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" wire:click="toggleSoloProfesor">
-                            {{ $solo_profesor ? 'Ver todos los profesores' : 'Ver solo mis horarios' }}
+                            {{ $solo_profesor ? 'Todos' : 'Mis horarios' }}
                         </button>
                     </div>
                     <div class="flex items-center justify-center">
